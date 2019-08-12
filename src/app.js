7677e26bfcab048e65876c855f7ae8135fe0103a
Misha Belokon <misha.belokon.3@gmail.com>
2019-08-12 15:35:41 +0300

    cleanup

---
+++ src/app.js
@@ -0,0 +1,20 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text> HELLO </Text>
+    </View>
+  );
+};
+
+export default App;
